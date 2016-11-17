17851fdcb5dce0d3ebd3c0a5c5ee801265e64639
Pedro Marquez <pedro.f.marquez.soto@accenture.com>
2016-11-16 22:09:32 -0600

    Added dependencies project

--- src/main/java/com/untitled/web/DemoApplication.java
+++ src/main/java/com/untitled/web/DemoApplication.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.untitled.util.TestUtil;
+
 @Controller
 public class DemoApplication {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
+		TestUtil util = new TestUtil();
+		
 		return "Hello World!";
 	}
 
---
+++ utils/src/main/java/com/untitled/util/TestUtil.java
@@ -0,0 +1,5 @@
+package com.untitled.util;
+
+public class TestUtil {
+
+}
