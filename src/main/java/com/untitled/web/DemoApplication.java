ea0258c9856375f4420ba274d4e2e527d3b5bf9e
Pedro Marquez <pedro.f.marquez.soto@accenture.com>
2016-11-16 22:36:57 -0600

    Remove unnecessary files

--- src/main/java/com/untitled/web/DemoApplication.java
+++ src/main/java/com/untitled/web/DemoApplication.java
@@ -4,14 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.untitled.util.TestUtil;
-
 @Controller
 public class DemoApplication {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		TestUtil util = new TestUtil();
+		//TestUtil util = new TestUtil();
 		
 		return "Hello World!";
 	}
--- utils/src/main/java/com/untitled/util/TestUtil.java
+++
@@ -1,5 +0,0 @@
-package com.untitled.util;
-
-public class TestUtil {
-
-}
